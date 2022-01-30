07a8f83e9e2b488151879fd1a2dd7b236dee975e
RobinMeneust <85349121+RobinMeneust@users.noreply.github.com>
2022-01-30 14:01:45 +0100

    Add files via upload

--- Projet Syracuse/script.bash
+++ Projet Syracuse/script.bash
@@ -2,8 +2,8 @@
 
 dir="output_dir"
 
-#We check if we have only two parameters that are integers
-if [ $# -ne 2 -o -n "${1//[0-9]/}" -o -n "${2//[0-9]/}" ]
+#We check if the two firsts parameters are integers
+if [ $# -ne 2 -o -n "${1//[0-9]/}" -o -n "${2//[0-9]/}" -o "${1:0:1}" = "0" -o "${2:0:1}" = "0" ]
 then
     #Display help
     echo "MANUAL: ..."
@@ -15,15 +15,15 @@ then
     exit 1
 fi
 
-date > "${dir}/logs.txt" #add the date at the beginning of the logs file
+#date > "${dir}/logs.txt" #add the date at the beginning of the logs file
 
-if ! [ -d $dir ]
+if ! [ -d $dir ] #if "dir" isn't a directory yet, we create it
 then
     mkdir $dir
 fi
 
-for u0 in `seq $1 $2`
+for u0 in `seq $1 $2` #in the directory, we create $2-$1 files for each number of Un between these two numbers
 do
-    nom_fichier="f${u0}.dat"
-    ./syracuse "$u0" "${dir}/${nom_fichier}" 2>>"${dir}/logs.txt"
+    file_name="f${u0}.dat"
+    ./syracuse "$u0" "${dir}/${file_name}" #2>>"${dir}/logs.txt"
 done
--- Projet Syracuse/syracuse.c
+++ Projet Syracuse/syracuse.c
@@ -7,11 +7,11 @@ long stringToLong(char * text)
     long number=0;
     long result=0;
 
-    while(text[i]!='\0'){
-        number=text[i]-'0';
+    while(text[i]!='\0'){ //we verify that the parameter is a number
+        number=text[i]-'0'; //by doing the difference between the parameter in ascii and 0 in ascii
         if(number<0 || number>9)
             return -1;
-        result=(result*10L) + number;
+        result=(result*10L) + number; //we transform the parameter in character(s) into a number
         i++;
     }
     return result;
@@ -21,33 +21,33 @@ int main(int argc, char** argv)
 {
     FILE* fileOutput = NULL;
 
-    long max_altitude=0;
-    long flight_duration=0;
-    long altitude_duration=0;
-    long altitude_duration_temp=0;
-    long un=0;
-    long u0=0;
+    long max_altitude=0;    //the maximum value of Un for each n
+    long flight_duration=0;     //the number n at the end of each flight
+    long altitude_duration=0;       //the maximum number of successives values up to each Un 
+    long altitude_duration_temp=0;      //?
+    long un=0;      //un is the actual altitude of the flight
+    long u0=0;      //u0 is the number to begin the flight
 
-    if(argc!=3){
+    if(argc!=3){ //if there ain't exactly 3 parameters, we quit the program
         fprintf(stderr, "ERROR: bad parameters\n");
         return 1;
     }
-    else if(strlen(argv[2])>FILENAME_MAX) //argv[1]>0 ?
+    else if(strlen(argv[2])>FILENAME_MAX) //argv[1]>0 ? //or if the parameters' names are to long
     {
         fprintf(stderr, "ERROR: the given file name is too long\n");
         return 1;
     }
     
 
-    u0=stringToLong(argv[1]);
+    u0=stringToLong(argv[1]); //we transform the first parameter which is in text string into a number
 
-    if(u0 <= 0)
+    if(u0 <= 0) //Syracuse works with strictly postive numbers
     {
         fprintf(stderr, "ERROR: U0='%s' is not correct\n", argv[1]);
         return 1;
     }
 
-    fileOutput = fopen(argv[2], "w");
+    fileOutput = fopen(argv[2], "w"); //we do the same checking for the second parameter
 
     if(!fileOutput){
         fprintf(stderr, "ERROR: the file can't be opened, it may be because of invalid characters\n");
@@ -57,13 +57,13 @@ int main(int argc, char** argv)
 
     un=u0;
 
-
+    //time to write in new files and prepare the columns n and Un
     fprintf(fileOutput,"n Un\n");
     fprintf(fileOutput, "0 %ld\n", u0);
 
     while(un!=1) // or: stop when there is a cycle ?
     {
-        if(un%2 == 0)
+        if(un%2 == 0) //if the numer is an even number
             un = un/2;
         else
             un = (un*3) + 1;
@@ -83,10 +83,11 @@ int main(int argc, char** argv)
         
         fprintf(fileOutput, "%ld %ld\n", flight_duration, un);
     }
+    //we write the results of records at the end of the file
     fprintf(fileOutput, "altimax=%ld\ndureevol=%ld\ndureealtitude=%ld", max_altitude, flight_duration, altitude_duration);
 
     
-    if(fclose(fileOutput)==EOF){
+    if(fclose(fileOutput)==EOF){ //time to close the file
         fprintf(stderr, "ERROR: the file can't be closed\n");
         return 1;
     }
